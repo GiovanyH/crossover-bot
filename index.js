1270fc77c8515e58b999bcb1f72fe51bc7e778cf
Gabriel Maximo <gbielmaximo@gmail.com>
2021-02-16 23:53:41 +0000

    acho q tá pronto a 1.0

--- index.js
+++ index.js
@@ -2,28 +2,72 @@ const { Client } = require("discord.js");
 const client = new Client(); 
 require('dotenv').config();
 
-
 let config = {
     prefix: "c:"
 }
 
-let servers = {}
+let totalServers = []
+
+let configuredServers = []
 
 client.on("ready", async () =>  {
-        console.log('conectado em ',Object.keys(client)," servers")
+    var getGuilds = new Promise((resolve,reject)=>{
+        let servers = []
+        client.guilds.cache.forEach((guild) => {
+            servers.push(guild)
+        })//.join("\n")
+        resolve(servers)
+    })
+    getGuilds.then(async (resp)=>{
+        totalServers = resp
+        console.log('conectado em ',resp.length," servers")
+    })
 });
 
 client.on("message", async message =>{
         const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
         const comando = args.shift().toLowerCase()
+
         if(message.content.indexOf(config.prefix) == 0) { 
-        switch(comando){
-            case "ping":
-                const m = await message.channel.send("Ping?");
-                console.log("ping")
-                m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
+            switch(comando){
+                case "ping":
+                    const m = await message.channel.send("Ping?");
+                    console.log("ping")
+                    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
+                    break;
+
+                case "servers":
+                    let names = []
+                    for ( guild in totalServers ) {
+                        names.push(totalServers[guild].name)
+                    }
+                    message.channel.send("Servers: "+names)
+                    break; 
+
+                case "connect":
+                    if(!configuredServers[message.guild.id]) {
+                        configuredServers[message.guild.id] = message.channel.id
+                        await message.channel.send("este chat foi conectado com o crossover")
+                    } else {
+                        await message.channel.send("este chat já está no crossover '-' ")
+                    }
                 break;
+
+            }
         }
+
+        // parte de enviar pra todos fica aqui
+        else if (configuredServers[message.guild.id] == message.channel.id && !message.author.bot) {
+            for ( key in configuredServers) {
+                const value = configuredServers[key]
+                if (key != message.guild.id) {
+                    const server = client.guilds.cache.get(key)
+                    const channel = server.channels.cache.get(value)
+                    await channel.send(message.author.username+": "+message.content)
+
+                }
+            }
         }
 })
 
